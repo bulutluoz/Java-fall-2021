fc8c80262065dca7eea8759f431ef2ba901566c0
bulutluoz <abbulutluoz@gmail.com>
2021-11-20 22:21:45 +0100

    21.gun sonu

--- src/day21_scope_arrays/C05_Arrays.java
+++ src/day21_scope_arrays/C05_Arrays.java
@@ -11,9 +11,11 @@ public class C05_Arrays {
 		
 		
 		int arr[]= {1,2,3,4,5,6,8,90};
+		
+		
 		int kova=arr[0];
 		
-		for (int i = 0; i < arr.length-5; i++) {
+		for (int i = 0; i < arr.length-1; i++) {
 			
 			arr[i]=arr[i+1];
 			
--- src/day21_scope_arrays/C06_Arrays.java
+++ src/day21_scope_arrays/C06_Arrays.java
@@ -1,9 +1,49 @@
 package day21_scope_arrays;
 
+import java.util.Arrays;
+
 public class C06_Arrays {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Soru 2: Verilen bir array’in tum elemanlarini toplayan bir program yazalim.
+
+		int arr[] = {3,5,8,5,2,4};
+		
+		int toplam=0;
+		
+		for (int i = 0; i < arr.length; i++) {
+			
+			toplam+=arr[i];
+		}
+		
+		
+		System.out.println("elementlerin toplami : " + toplam);
+		
+		
+		// array'in tum elemanlarini once klucukten buyuge, sonra buyukten kucuge yazdirin
+		
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));
+		
+		//buyukten kucuge yazdirmak icin
+		// ya for loop ile elemanlari ters sirada yazdiririz
+		
+		for (int i = arr.length-1; i >=0 ; i--) {
+			
+			System.out.print(arr[i] + " ");
+		}
+		
+		//array yapmak isterseniz ayni uzunlukta bir array olusturup
+		// elemanlari tesr sirada yeni arraye tasiyabilirsiniz
+		
+		System.out.println(" ");
+		
+		int tersArray[]=new int[arr.length];
+		
+		for (int i = 0; i < tersArray.length; i++) {
+			tersArray[i]=arr[arr.length-1-i];
+		}
+		System.out.println(Arrays.toString(tersArray));
 
 	}
 
