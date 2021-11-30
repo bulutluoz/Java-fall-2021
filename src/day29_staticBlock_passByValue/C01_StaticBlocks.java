2cda0aa6be6e54c7301537337ea27a9ed53b1161
bulutluoz <abbulutluoz@gmail.com>
2021-11-30 20:57:30 +0100

    29.gun ara

---
+++ src/day29_staticBlock_passByValue/C01_StaticBlocks.java
@@ -0,0 +1,19 @@
+package day29_staticBlock_passByValue;
+
+public class C01_StaticBlocks {
+	
+	
+	static {
+		System.out.println("static blok main method'dan bile once calisir ");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Javada once main method calisir");
+
+	}
+	
+	static {
+		System.out.println("static blok nereye yazilirsa yazilsin main method'dan bile once calisir ");
+	}
+
+}
