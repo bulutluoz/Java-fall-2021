3d670a5211b5fa284b528b1a3ac26dd86252a696
bulutluoz <abbulutluoz@gmail.com>
2021-12-09 20:53:37 +0100

    37. ders ara

---
+++ src/day37_Inheritance/Araba.java
@@ -0,0 +1,5 @@
+package day37_Inheritance;
+
+public class Araba {
+
+}
---
+++ src/day37_Inheritance/Corolla.java
@@ -0,0 +1,10 @@
+package day37_Inheritance;
+
+public class Corolla {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
